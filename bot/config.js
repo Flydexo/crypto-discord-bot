8ec3849885e42d0b16a3a7daa6cc78e8ef48adf7
Flydexo <flydexo@gmail.com>
2021-06-20 09:30:54 +0200

    updates

---
+++ bot/config.js
@@ -0,0 +1,10 @@
+module.exports.TOKEN = "ODUzMTYxNjA2MDg2MzkzODY2.YMRWqQ.XRgJZyt3CEp_L97aN_mRcHGyeJw";
+module.exports.memberMultiplier = 1;
+module.exports.messageLength = 2;
+module.exports.channels = {
+    talk: "855150382296203285",
+    block: "855179123176636456",
+    order: "855179082235904050",
+    exchange: "855151005125836800"
+};
+module.exports.guild = "852938111255576587";
