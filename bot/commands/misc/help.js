0bd6b10c9464a89bf15f98697c627fc21cc6c0bf
Flydexo <flydexo@gmail.com>
2021-06-20 10:03:55 +0200

    updates

--- bot/commands/misc/help.js
+++ bot/commands/misc/help.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args) => {
-
+    message.reply("https://discord.com/channels/852938111255576587/855149527559569459/855175800449794089");
 }
 
 module.exports.help = {
